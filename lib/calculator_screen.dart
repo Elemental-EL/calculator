a4120ef5d031f39a422476b6ec44c2186f6654a9
Elyar KordKatool <elyar.k@yahoo.com>
2024-06-09 14:18:41 +0330

    Back-End is done(more changes to be made...)

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -1,7 +1,9 @@
+
 import 'package:calculator/button_values.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
+import 'package:decimal/decimal.dart';
 class CalculatorScreen extends StatefulWidget {
   const CalculatorScreen({super.key});
 
@@ -10,6 +12,9 @@ class CalculatorScreen extends StatefulWidget {
 }
 
 class _CalculatorScreenState extends State<CalculatorScreen> {
+  String number1 ="";
+  String operand ="";
+  String number2 ="";
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
@@ -19,18 +24,29 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
         child: Column(children: [
           //output
           Expanded(
-            child: SingleChildScrollView(
-              reverse: true,
-              child: Container(
+            child: Scrollbar(
+              thumbVisibility: true,
+              radius: const Radius.circular(5),
+              thickness: 5,
+              child: Align(
                 alignment: Alignment.bottomRight,
-                padding: const EdgeInsets.all(16),
-                child: const Text(
-                  "0",
-                  style: TextStyle(
-                  fontSize: 48,
-                  fontWeight: FontWeight.bold,
+                child: SingleChildScrollView(
+                  scrollDirection: Axis.horizontal,
+                  reverse: true,
+                  child: Container(
+                    padding: const EdgeInsets.all(16),
+                    child: Align(
+                      alignment: Alignment.bottomRight,
+                      child: Text(
+                        "$number1$operand$number2".isEmpty?"0":"$number1$operand$number2",
+                        style: const TextStyle(
+                        fontSize: 48,
+                        fontWeight: FontWeight.bold,
+                        ),
+                        textAlign: TextAlign.end,
+                      ),
+                    ),
                   ),
-                  textAlign: TextAlign.end,
                 ),
               ),
             ),
@@ -66,13 +82,163 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
           borderRadius: BorderRadius.circular(25),
         ),
         child: InkWell(
-          onTap: () {},
+          onTap: () =>onBtnTap(value),
           child: Center(
               child: Text(value , style: const TextStyle(fontWeight: FontWeight.bold, fontSize: 24),)
           ),
         ),
       ),
     );
+  }
+
+  void onBtnTap(String value){
+    clearError();
+    if(value==Btn.del){
+      delete();
+      return;
+    }
+    if (value==Btn.clr){
+      clearAll();
+      return;
+    }
+    if (value==Btn.per){
+      convertToPercentage();
+      return;
+    }
+    if (value==Btn.calculate){
+      calculate();
+      return;
+    }
+    appendValue(value);
+  }
+
+  void calculate(){
+    if (number1.isEmpty) return;
+    if (operand.isEmpty) return;
+    if (number2.isEmpty) return;
+    Decimal num1 = Decimal.parse(number1);
+    Decimal num2 = Decimal.parse(number2);
+    Decimal res = Decimal.parse('0.0');
+    switch (operand){
+      case Btn.add:
+        res = num1+num2;
+        break;
+      case Btn.subtract:
+        res = num1-num2;
+        break;
+      case Btn.multiply:
+        res = num1*num2;
+        break;
+      case Btn.divide:
+        if (number2!=Btn.n0){
+          res = (num1/num2).toDecimal(scaleOnInfinitePrecision: 20);
+        }
+        break;
+      default:
+    }
+    setState(() {
+      if (number2==Btn.n0&&operand==Btn.divide){
+        number1 = "Error";
+      } else {
+        number1 = res.toString();
+      }
+      if(number1.endsWith(".0")){
+        number1 = number1.substring(0,number1.length-2);
+      }
+      number2="";
+      operand="";
+    });
+  }
+
+  void clearError(){
+    if (number1=="Error"){
+      setState(() {
+        number1="";
+        number2="";
+        operand="";
+      });
+      return;
+    }
+  }
+
+  void convertToPercentage(){
+    if (number1.isNotEmpty&&operand.isNotEmpty&&number2.isNotEmpty){
+      calculate();
+    }
+    if (operand.isNotEmpty){
+      setState(() {
+        number1="Error";
+        number2="";
+        operand="";
+      });
+      return;
+    }
+    Decimal num = Decimal.parse(number1);
+    Decimal per = Decimal.parse('100');
+    Decimal res = (num/per).toDecimal();
+    setState(() {
+      number1 =res.toString();
+      operand="";
+      number2="";
+    });
+  }
+
+  void clearAll(){
+    setState(() {
+      number1="";
+      number2="";
+      operand="";
+    });
+  }
+
+  void delete(){
+    clearError();
+    if (number2.isNotEmpty){
+      number2=number2.substring(0,number2.length-1);
+    } else if (operand.isNotEmpty){
+      operand="";
+    } else if(number1.isNotEmpty){
+      number1=number1.substring(0,number1.length-1);
+    }
+    setState(() {
+
+    });
+  }
+
+
+
+  void appendValue (String value){
+    clearError();
+    if (number1.isEmpty&&value==Btn.n0) return;
+    if (number1.isEmpty&&value==Btn.add) return;
+    if (value!=Btn.dot && int.tryParse(value)==null){
+      if (operand.isNotEmpty&&number2.isNotEmpty){
+        calculate();
+      }
+      if (number1.isEmpty&&value==Btn.subtract) {
+        number1 = value;
+      }else if(number1.isNotEmpty&&(operand==Btn.multiply||operand==Btn.divide)&&number2.isEmpty&&value==Btn.subtract){
+        number2 = value;
+      }
+      else {
+        operand = value;
+      }
+    } else if(number1.isEmpty || operand.isEmpty){
+      if (value==Btn.dot&&number1.contains(Btn.dot)) return;
+      if (value==Btn.dot&&(number1.isEmpty || number1==Btn.n0)) {
+        value = "0.";
+      }
+      number1 += value;
+    } else if(number2.isEmpty || operand.isNotEmpty){
+      if (value==Btn.dot&&number2.contains(Btn.dot)) return;
+      if (value==Btn.dot&&(number2.isEmpty || number2==Btn.n0)) {
+        value = "0.";
+      }
+      number2 += value;
+    }
+    setState(() {
+
+    });
   }
 
   Color getBtnColor(value){
