c0ae039c3295dd8aebd149b3644d23d28032f41c
Elyar KordKatool <elyar.k@yahoo.com>
2024-07-01 19:35:09 +0330

    Qol update.

--- lib/calculator_screen.dart
+++ lib/calculator_screen.dart
@@ -15,6 +15,28 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   String number1 ="";
   String operand ="";
   String number2 ="";
+
+  late ScrollController _scrollController;
+
+  @override
+  void initState() {
+    super.initState();
+    _scrollController = ScrollController();
+    WidgetsBinding.instance.addPostFrameCallback((_) {
+      _scrollController.jumpTo(_scrollController.position.minScrollExtent);
+    });
+  }
+
+  @override
+  void dispose() {
+    _scrollController.dispose();
+    super.dispose();
+  }
+
+  double _maxScrollPosition = 0.0;
+
+  bool lastPressedWasCalc = false;
+
   @override
   Widget build(BuildContext context) {
     final screenSize = MediaQuery.of(context).size;
@@ -28,29 +50,31 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
               thumbVisibility: true,
               radius: const Radius.circular(5),
               thickness: 5,
+              controller: _scrollController,
               child: Align(
                 alignment: Alignment.bottomRight,
-                child: SingleChildScrollView(
-                  scrollDirection: Axis.horizontal,
-                  reverse: true,
-                  child: Container(
-                    padding: const EdgeInsets.all(16),
-                    child: Align(
-                      alignment: Alignment.bottomRight,
-                      child: Text(
-                        "$number1$operand$number2".isEmpty?"0":"$number1$operand$number2",
-                        style: const TextStyle(
-                        fontSize: 48,
-                        fontWeight: FontWeight.bold,
+                  child: SingleChildScrollView(
+                    scrollDirection: Axis.horizontal,
+                    reverse: true,
+                    controller: _scrollController,
+                    child: Container(
+                      padding: const EdgeInsets.all(16),
+                      child: Align(
+                        alignment: Alignment.bottomRight,
+                        child: Text(
+                          "$number1$operand$number2".isEmpty?"0":"$number1$operand$number2",
+                          style: const TextStyle(
+                          fontSize: 48,
+                          fontWeight: FontWeight.bold,
+                          ),
+                          textAlign: TextAlign.end,
                         ),
-                        textAlign: TextAlign.end,
                       ),
                     ),
                   ),
                 ),
               ),
             ),
-          ),
           
           //input
           Wrap(
@@ -113,9 +137,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
   }
 
   void calculate(){
-    if (number1.isEmpty) return;
-    if (operand.isEmpty) return;
-    if (number2.isEmpty) return;
+    if (number1.isEmpty||operand.isEmpty||number2.isEmpty) return;
     Decimal num1 = Decimal.parse(number1);
     Decimal num2 = Decimal.parse(number2);
     Decimal res = Decimal.parse('0.0');
@@ -131,7 +153,7 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
         break;
       case Btn.divide:
         if (number2!=Btn.n0){
-          res = (num1/num2).toDecimal(scaleOnInfinitePrecision: 20);
+          res = (num1/num2).toDecimal(scaleOnInfinitePrecision: 10);
         }
         break;
       default:
@@ -147,13 +169,20 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
       }
       number2="";
       operand="";
+      _maxScrollPosition = _scrollController.position.maxScrollExtent+1000;
     });
+    _scrollController.animateTo(
+      _maxScrollPosition,
+      duration: const Duration(milliseconds: 500),
+      curve: Curves.ease,
+    );
+    lastPressedWasCalc = true;
   }
 
   void clearError(){
     if (number1=="Error"){
       setState(() {
-        number1="";
+        number1="0";
         number2="";
         operand="";
       });
@@ -218,17 +247,27 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
       if (number1.isEmpty&&value==Btn.subtract) {
         number1 = value;
       }else if(number1.isNotEmpty&&(operand==Btn.multiply||operand==Btn.divide)&&number2.isEmpty&&value==Btn.subtract){
-        number2 = value;
+          number2 = value;
       }
       else {
-        operand = value;
+        if (number1!="Error") {
+          operand = value;
+        }
       }
     } else if(number1.isEmpty || operand.isEmpty){
       if (value==Btn.dot&&number1.contains(Btn.dot)) return;
       if (value==Btn.dot&&(number1.isEmpty || number1==Btn.n0)) {
         value = "0.";
       }
-      number1 += value;
+      if (lastPressedWasCalc&&value==Btn.dot){
+        number1 = "0.";
+        lastPressedWasCalc = false;
+      } else if (lastPressedWasCalc){
+        number1 = value;
+        lastPressedWasCalc = false;
+      } else {
+        number1 += value;
+      }
     } else if(number2.isEmpty || operand.isNotEmpty){
       if (value==Btn.dot&&number2.contains(Btn.dot)) return;
       if (value==Btn.dot&&(number2.isEmpty || number2==Btn.n0)) {
@@ -237,8 +276,13 @@ class _CalculatorScreenState extends State<CalculatorScreen> {
       number2 += value;
     }
     setState(() {
-
+      _maxScrollPosition = _scrollController.position.minScrollExtent;
     });
+    _scrollController.animateTo(
+      _maxScrollPosition,
+      duration: const Duration(milliseconds: 200),
+      curve: Curves.ease,
+    );
   }
 
   Color getBtnColor(value){
